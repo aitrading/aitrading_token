e8009fc75495a6d5d04b6da6ce381e926b345013
cotyar <khabarov@gmail.com>
2018-06-25 20:14:15 +0100

    Initial supply increased to 108 888 888

--- contracts/AITTToken.sol
+++ contracts/AITTToken.sol
@@ -7,7 +7,7 @@ contract AITTToken is CrowdsaleToken {
     string public constant name = "AI Trading Token";
     string public constant symbol = "AITT";
     uint public constant decimals = 18;
-    uint public constant initialSupply = 67888888 * 10 ** 18;
+    uint public constant initialSupply = 108888888 * 10 ** 18;
 
     function AITTToken(address _teamMultisigWallet) public CrowdsaleToken(_teamMultisigWallet, name, symbol, initialSupply, decimals, false) {
     }
